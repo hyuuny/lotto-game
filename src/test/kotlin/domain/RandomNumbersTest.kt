30a6babe419c69313e032143ce9e3c47a6cf1c61
hyuuny <shyune@knou.ac.kr>
2022-07-05 18:51:33 +0900

    feat: 로또 번호 생성 테스트 추가

---
+++ src/test/kotlin/domain/RandomNumbersTest.kt
@@ -0,0 +1,33 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import java.util.*
+import kotlin.test.Test
+
+class RandomNumbersTest {
+
+    @Test
+    internal fun `랜덤 로또 번호 6개 가져오기`() {
+        val numbers = Numbers()
+        val randomNumbers = numbers.randomNumbers
+        assertThat(randomNumbers.size).isEqualTo(6)
+    }
+
+}
+
+
+const val MAX_NUMBER = 6
+const val MAX_LOTTO_NUMBER = 45
+
+class Numbers {
+
+    val randomNumbers: List<Int>
+        get() {
+            val randomNumbers = mutableSetOf<Int>()
+            while (randomNumbers.size < MAX_NUMBER) {
+                randomNumbers.add(Random().nextInt(MAX_LOTTO_NUMBER) + 1)
+            }
+            return randomNumbers.sorted()
+        }
+
+}
